985406a185a8b19a066ed7a217038b42674b0728
lotusxhaofda001 <85655882+lotusxhaofda001@users.noreply.github.com>
2021-09-30 13:28:32 +0800

    Update jd_cfd_loop.js

--- jd_cfd_loop.js
+++ jd_cfd_loop.js
@@ -46,7 +46,7 @@ $.appId = 10028;
   while (count < loop_limit) {
     count++
     console.log(`============开始第${count}次挂机=============`)
-    for (let i = 0; i < cookiesArr.length; i++) {
+    for (let i = 0; i < cookiesArr.length - 1; i++) {
       if (cookiesArr[i]) {
         cookie = cookiesArr[i];
         $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
